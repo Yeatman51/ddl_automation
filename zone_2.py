ee827f5a7129c2b25370df556a6124b4a7bb386b
jon <jonlyeatman@gmail.com>
2022-06-10 16:08:32 -0400

    working on zone 2

---
+++ zone_2.py
@@ -0,0 +1,85 @@
+# Python script to Control your chrome browser using selenium 
+# RUN pip install selenium  https://pypi.org/project/selenium/
+# download selenium driver from https://chromedriver.chromium.org/home 
+
+import time, os
+from turtle import up
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+load_dotenv()
+
+# .env
+chrome_path = "/Users/jonyeatman/Downloads/chromedriver"
+email = os.environ.get("EMAIL")
+password  = os.environ.get("PASSWD")
+url = os.environ.get("URL")
+
+# zone 2 KZ
+ddl1 = 'Fridge 24'
+ddl2 = 'Fridge 25'
+ddl3 = 'Fridge 26'
+ddl4 = 'Freezer 22'
+ddl5 = 'Freezer 23'
+date = 'June 7, 2022'
+
+
+print("process Started ---------")
+
+op = Options()
+op.add_argument("--start-maximized") #open Browser in maximized mode
+op.add_argument("--no-sandbox") #bypass OS security model
+op.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
+# op.add_experimental_option("excludeSwitches", ["enable-automation"])
+# op.add_experimental_option('useAutomationExtension', False)
+op.headless = False # change to true if we want to hide the browser
+s = Service(chrome_path)
+
+with  webdriver.Chrome(service=s, options=op) as d:
+   d.get(url)
+   d.find_element(by=By.NAME, value='login-form:j_idt120').send_keys(email)
+   d.find_element(by=By.NAME, value='login-form:j_idt125').send_keys(password)
+   d.find_element(by=By.ID, value='login-form:j_idt127').click()
+   time.sleep(2)
+
+
+# ddl input
+   d.find_element(by=By.XPATH, value='//*[@id="intempconnect-data-devices-form:config-name-filter-table:0:devices-panel-config-name-filter-input"]').send_keys(ddl1)
+   time.sleep(2)
+# drop-down
+   d.find_element(by=By.XPATH, value='//*[@id="intempconnect-data-devices-form:date-filter-table:0:devices-panel-date-filter-input"]').send_keys('b')
+   time.sleep(2)
+   d.find_element(by=By.XPATH, value='//*[@id="intempconnect-data-devices-form:date-filter-table:0:devices-panel-date-filter-input"]').send_keys('b')
+   time.sleep(2)
+# date input
+   d.find_element(by=By.XPATH, value='//*[@id="intempconnect-data-devices-form:date-filter-table:0:devices-panel-date-to-filter-input_input"]').send_keys(date)
+   time.sleep(4)
+# data table
+# Matching value text
+   # d.find_element(by=By.LINK_TEXT, value=f'{ddl1}')
+   d.find_element(by=By.LINK_TEXT, value=f'{ddl1}').click()
+   time.sleep(3)
+# export
+   if d.find_element(by=By.LINK_TEXT, value='Export').click():
+      time.sleep(3)
+   # run report
+      d.find_element(by=By.ID, value='intempconnect-data-builder-form:runBtn').click()
+      print(f'Downloaded {ddl1} from {date}')
+      time.sleep(12)
+# home
+   else: 
+      d.find_element(by=By.ID, value='j_idt40:j_idt42').click()
+      time.sleep(3)
+      print(f'Error Missing {ddl1} from {date}')
+
+   d.find_element(by=By.ID, value='j_idt40:j_idt42').click()
+   time.sleep(3)
+# rerun
+
+
+
+print("Done----------------------")
